1c5322d1a95cb6480d1b430549d73ce6b8c12e6f
Andrew Hanasiro <andrewkhanasiro@gmail.com>
2019-11-27 15:22:55 -0300

    init

---
+++ server/database/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE
+IF NOT EXISTS `user` (
+    `id` INT
+(20) NOT NULL AUTO_INCREMENT,
+    
+);
---
+++ server/src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import app from "./app.js";
+
+const server = express();
+
+server.use(app);
+
+server.listen(8080, () => {
+    console.warn('Server running on: 8080');
+});
