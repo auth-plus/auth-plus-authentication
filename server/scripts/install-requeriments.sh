29fc66f2af15972c3415b5f7097db02534452488
AndrewHanasiro <andrewkhanasiro@gmail.com>
2020-09-20 08:43:48 -0300

    feat: fix makefile and install sh

--- server/scripts/install-requeriments.sh
+++ server/scripts/install-requeriments.sh
@@ -18,3 +18,9 @@ sudo usermod -aG docker $USER
 # Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# Node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install --lts
