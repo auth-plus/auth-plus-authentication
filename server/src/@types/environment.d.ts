ff633db5b165165135e546b48fdfdc578736eea7
AndrewHanasiro <andrewkhanasiro@gmail.com>
2020-09-26 19:53:31 -0300

    feat: docker-compose wokring

---
+++ server/src/@types/environment.d.ts
@@ -0,0 +1,9 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      APP_PORT?: string;
+    }
+  }
+}
+
+export {};
--- server/src/index.ts
+++ server/src/index.ts
@@ -9,14 +9,11 @@ app.get("/", (req: Request, res: Response) => {
   res.status(200).send("Ok");
 });
 
-//Handling 404
 app.use((req: Request, res: Response) => {
-  res.status(404).send("Sorry cant find that!");
+  res.status(404).send("Sorry cant find that");
 });
 
-
-
-const PORT = 5000;
+const PORT = process.env.APP_PORT ? parseInt(process.env.APP_PORT) : 5000;
 app.listen(PORT, () => {
   console.warn(`Server running on: ${PORT}`);
 });
