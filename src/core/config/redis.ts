ea3f52a94659fa088375b61536d4d56218c9b00a
Andrew Hanasiro <andrewkhanasiro@gmail.com>
2021-06-20 18:15:44 -0300

    feat: readis with pw (#3)

--- src/core/config/redis.ts
+++ src/core/config/redis.ts
@@ -2,7 +2,13 @@ import { promisify } from 'util'
 
 import redis from 'redis'
 
-const client = redis.createClient()
+import config from './enviroment_config'
+
+const client = redis.createClient({
+  host: config.cache.host,
+  port: config.cache.port,
+  password: config.cache.password,
+})
 const getAsync = promisify(client.get).bind(client)
 const setAsync = promisify(client.set).bind(client)
 
