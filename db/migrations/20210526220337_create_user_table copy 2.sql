d6ba7c4c16796a11c18113198ea179466e2d307e
AndrewHanasiro <andrewkhanasiro@gmail.com>
2021-06-20 22:25:07 -0300

    fix: migrations

--- db/migrations/20210526220337_create_user_table copy 2.sql
+++ db/migrations/20210526220337_create_user_table copy 2.sql
@@ -5,20 +5,24 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS "user" (
     "id" UUID not null default uuid_generate_v1() PRIMARY KEY,
     "name" varchar(128) not null,
-    "email" varchar(64) not null UNIQUE,
-    "password_hash" text not null
+    "email" varchar(64) not null uNIQUE,
+    "phone" varchar(16) default null,
+    "password_hash" text not null,
+    "created_at" timestamp not null default current_timestamp
 );
 
 CREATE TABLE IF NOT EXISTS "organization" (
     "id" UUID not null default uuid_generate_v1() PRIMARY KEY,
     "name" varchar(128) not null,
     "document" varchar(64) not null,
-    "document_type" varchar(32) not null
+    "document_type" varchar(32) not null,
+    "created_at" timestamp not null default current_timestamp
 );
 
 CREATE TABLE IF NOT EXISTS "administrator" (
     "id" UUID not null default uuid_generate_v1() PRIMARY KEY,
     "user_id" UUID not null,
+    "created_at" timestamp not null default current_timestamp,
     CONSTRAINT fk_administrator_user
       FOREIGN KEY("user_id") 
 	  REFERENCES "user"("id")
@@ -28,6 +32,7 @@ CREATE TABLE IF NOT EXISTS "organization_manager" (
     "id" UUID not null default uuid_generate_v1() PRIMARY KEY,
     "user_id" UUID not null,
     "organization_id" UUID not null,
+    "created_at" timestamp not null default current_timestamp,
     CONSTRAINT fk_om_user
       FOREIGN KEY("user_id") 
 	  REFERENCES "user"("id"),
@@ -40,6 +45,7 @@ CREATE TABLE IF NOT EXISTS "organization_user" (
     "id" UUID not null default uuid_generate_v1() PRIMARY KEY,
     "user_id" UUID not null,
     "organization_id" UUID not null,
+    "created_at" timestamp not null default current_timestamp,
     CONSTRAINT fk_ou_user
       FOREIGN KEY("user_id") 
 	  REFERENCES "user"("id"),
--- db/migrations/20210527202631_create_mfa_table.sql
+++ db/migrations/20210527202631_create_mfa_table.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS "multi_factor_authentication" (
     "id" UUID not null default uuid_generate_v1(),
     "user_id" UUID not null,
     "strategy" varchar(32) not null,
+    "is_enable" boolean not null default TRUE,
     PRIMARY KEY ("id"),
     CONSTRAINT fk_mfa_user
       FOREIGN KEY("user_id") 
