856126730e6afca878a0643f7fea3fe25168c3ce
AndrewHanasiro <andrewkhanasiro@gmail.com>
2021-07-04 19:49:30 -0300

    test: add more case on login

--- test/unit/usecases/login.usecase.test.ts
+++ test/unit/usecases/login.usecase.test.ts
@@ -9,17 +9,20 @@ import { MFARepository } from '../../../src/core/providers/mfa.repository'
 import { MFAChooseRepository } from '../../../src/core/providers/mfa_choose.repository'
 import { FindingMFA } from '../../../src/core/usecases/driven/finding_mfa.driven'
 import { CreatingMFAChoose } from '../../../src/core/usecases/driven/creating_mfa_choose.driven'
+import { Strategy } from '../../../src/core/entities/strategy'
 
 describe('login usecase', function () {
   const userId = 'any-uuid'
   const email = 'test@test.com'
   const password = '123456'
+  const hash = 'any-hash-generated-by-service'
+  const strategyList = [Strategy.EMAIL]
   const user: User = {
     id: userId,
     name: 'test_user',
     email,
   }
-  it('should succeed when enter with right email but has no strategy list', async () => {
+  it('should succeed when enter with correct credential but has no strategy list', async () => {
     const mockFindingUser: FindingUser = mock(UserRepository)
     when(
       mockFindingUser.findUserByEmailAndPassword(email, password)
@@ -35,8 +38,34 @@ describe('login usecase', function () {
 
     const testClass = new Login(findingUser, findingMFA, creatingMFAChoose)
     const response = await testClass.login(email, password)
+
     verify(mockFindingUser.findUserByEmailAndPassword(email, password)).once()
+    verify(mockFindingMFA.findMFAByUserId(userId)).once()
     verify(mockCreatingMFAChoose.create(anything(), anything())).never()
     expect(response).to.eql(user)
+  })
+
+  it('should succeed when enter with correct credential with strategy list', async () => {
+    const mockFindingUser: FindingUser = mock(UserRepository)
+    when(
+      mockFindingUser.findUserByEmailAndPassword(email, password)
+    ).thenResolve(user)
+    const findingUser: FindingUser = instance(mockFindingUser)
+
+    const mockFindingMFA: FindingMFA = mock(MFARepository)
+    when(mockFindingMFA.findMFAByUserId(userId)).thenResolve(strategyList)
+    const findingMFA: FindingMFA = instance(mockFindingMFA)
+
+    const mockCreatingMFAChoose: CreatingMFAChoose = mock(MFAChooseRepository)
+    when(mockCreatingMFAChoose.create(userId, strategyList)).thenResolve(hash)
+    const creatingMFAChoose: CreatingMFAChoose = instance(mockCreatingMFAChoose)
+
+    const testClass = new Login(findingUser, findingMFA, creatingMFAChoose)
+    const response = await testClass.login(email, password)
+
+    verify(mockFindingUser.findUserByEmailAndPassword(email, password)).once()
+    verify(mockFindingMFA.findMFAByUserId(userId)).once()
+    verify(mockCreatingMFAChoose.create(userId, strategyList)).once()
+    expect(response).to.eql({ hash, strategyList })
   })
 })
