d3bb9b14db98ea7217a577643f895e5145576881
AndrewHanasiro <andrewkhanasiro@gmail.com>
2021-07-26 16:19:12 -0300

    feat: integration test

--- test/integration/login.route.test.ts
+++ test/integration/login.route.test.ts
@@ -5,37 +5,34 @@ import database from '../../src/core/config/database'
 import app from '../../src/index'
 
 describe('Login Route', () => {
+  const name = 'test'
+  const email = 'teste@test.com'
   let id: string
+
   before(async () => {
     const row: string[] = await database('user')
       .insert({
-        name: 'test',
-        email: 'teste@test.com',
-        password:
+        name,
+        email,
+        password_hash:
           '$2b$12$N5NbVrKwQYjDl6xFdqdYdunBnlbl1oyI32Uo5oIbpkaXoeG6fF1Ji',
       })
       .returning('id')
     id = row[0]
   })
+
+  after(async () => {
+    await database('user').where('id', id).del()
+  })
+
   it('should succeed when login', async () => {
-    request(app)
-      .post('/login')
-      .send({
-        email: 'teste@teste.com',
-        password: '7061651770d7b3ad8fa96e7a8bc61447',
-      })
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end((err, res) => {
-        if (err) {
-          throw err
-        } else {
-          expect(res).to.be.eql({
-            id,
-            name: 'test',
-            email: 'teste@teste.com',
-          })
-        }
-      })
+    const response = await request(app).post('/login').send({
+      email,
+      password: '7061651770d7b3ad8fa96e7a8bc61447',
+    })
+    expect(response.status).to.be.equal(200)
+    expect(response.body.id).to.be.equal(id)
+    expect(response.body.name).to.be.equal(name)
+    expect(response.body.email).to.be.equal(email)
   })
 })
