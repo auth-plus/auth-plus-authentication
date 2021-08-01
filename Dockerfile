1c0f94a828bb6f733e7dcb5e4e481ae363ca3f1d
Andrew Hanasiro <andrewkhanasiro@gmail.com>
2021-08-01 18:31:51 -0300

    Bugfix/eslint (#32) (#33)
    * feat: eslint
    
    test: tsconfig-path
    
    * chore: eslint now working with paths
    
    * fix: mocha require

--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,7 @@ RUN npm run build
 FROM node:14.17.0-alpine3.13 as deployable
 WORKDIR /app
 EXPOSE 5000
-COPY --from=builder /app/build ./
-COPY --from=dependency /app/node_modules ./node_modules
-CMD [ "node", "index.js" ]
+COPY ./package.json ./package.json 
+COPY --from=builder /app/build ./build
+RUN npm install ts-node@10.0.0 tsconfig-paths@3.10.1
+CMD ["npm", "start"]
--- src/core/services/password.service.ts
+++ src/core/services/password.service.ts
@@ -1,12 +1,12 @@
-import bcrypt from 'bcrypt'
+import { genSaltSync, hash, compare } from 'bcrypt'
 
 export class PasswordService {
   private round = 12
   async generateHash(password: string): Promise<string> {
-    const salt = bcrypt.genSaltSync(this.round)
-    return bcrypt.hash(password, salt)
+    const salt = genSaltSync(this.round)
+    return hash(password, salt)
   }
   async compare(password: string, hash: string): Promise<boolean> {
-    return bcrypt.compare(password, hash)
+    return compare(password, hash)
   }
 }
