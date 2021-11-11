8cf7cd4343d06d707062566c9f417682944e485d
AndrewHanasiro <andrewkhanasiro@gmail.com>
2021-11-11 09:52:10 -0300

    test: use cases

--- src/core/usecases/mfa.usecase.ts
+++ src/core/usecases/mfa.usecase.ts
@@ -1,19 +1,57 @@
-import { CreatingMFA } from './driven/creating_mfa.driven'
-import { ValidatingMFA } from './driven/validating_mfa.driven'
-import { CreateMFA, MFACreateInput } from './driver/create_mfa.driver'
+import {
+  CreatingMFA,
+  CreatingMFAErrorsTypes,
+} from './driven/creating_mfa.driven'
+import {
+  ValidatingMFA,
+  ValidatingMFAErrors,
+  ValidatingMFAErrorsTypes,
+} from './driven/validating_mfa.driven'
+import {
+  CreateMFA,
+  CreateMFAErrors,
+  CreateMFAErrorsTypes,
+  MFACreateInput,
+} from './driver/create_mfa.driver'
+import {
+  ValidateMFA,
+  ValidateMFAErrors,
+  ValidateMFAErrorsTypes,
+} from './driver/validate_mfa.driver'
 
-export default class MFA implements CreateMFA {
+export default class MFA implements CreateMFA, ValidateMFA {
   constructor(
     private creatingMFA: CreatingMFA,
     private validatingMFA: ValidatingMFA
   ) {}
 
   async create(content: MFACreateInput): Promise<string> {
-    const { name, userId, strategy } = content
-    return this.creatingMFA.creatingStrategyForUser(name, userId, strategy)
+    try {
+      const { name, userId, strategy } = content
+      return this.creatingMFA.creatingStrategyForUser(name, userId, strategy)
+    } catch (error) {
+      throw this.handleError(error as Error)
+    }
   }
 
   async validate(mfaId: string): Promise<boolean> {
-    return this.validatingMFA.validate(mfaId)
+    try {
+      return this.validatingMFA.validate(mfaId)
+    } catch (error) {
+      throw this.handleError(error as Error)
+    }
+  }
+
+  private handleError(error: Error) {
+    switch (error.message) {
+      case CreatingMFAErrorsTypes.ALREADY_EXIST:
+        return new CreateMFAErrors(CreateMFAErrorsTypes.WRONG_CREDENTIAL)
+      case CreatingMFAErrorsTypes.DATABASE_DEPENDECY_ERROR:
+        return new CreateMFAErrors(CreateMFAErrorsTypes.DEPENDECY_ERROR)
+      case ValidatingMFAErrorsTypes.NOT_FOUND:
+        return new ValidateMFAErrors(ValidateMFAErrorsTypes.WRONG_CREDENTIAL)
+      default:
+        return new Error('MFA unmapper error')
+    }
   }
 }
---
+++ test/unit/usecases/logout.usecase.test.ts
@@ -0,0 +1,122 @@
+import { expect } from 'chai'
+import faker from 'faker'
+import { mock, instance, when, verify, anything } from 'ts-mockito'
+
+import { User } from '../../../src/core/entities/user'
+import { TokenRepository } from '../../../src/core/providers/token.repository'
+import { UserRepository } from '../../../src/core/providers/user.repository'
+import {
+  FindingUser,
+  FindingUserErrorsTypes,
+} from '../../../src/core/usecases/driven/finding_user.driven'
+import {
+  InvalidatingToken,
+  InvalidatingTokenErrorsTypes,
+} from '../../../src/core/usecases/driven/invalidating_token.driven'
+import { LogoutUserErrorsTypes } from '../../../src/core/usecases/driver/logout_user.driver'
+import Logout from '../../../src/core/usecases/logout.usecase'
+
+describe('logout usecase', function () {
+  const userId = faker.datatype.uuid()
+  const name = faker.name.findName()
+  const email = faker.internet.email(name.split(' ')[0])
+  const user: User = {
+    id: userId,
+    name,
+    email,
+  }
+  it('should succeed when invalidate a single token', async () => {
+    const jwtPayload = {
+      userId: userId,
+    }
+    const token = faker.datatype.number(6).toString()
+
+    const mockFindingUser: FindingUser = mock(UserRepository)
+    when(mockFindingUser.findById(jwtPayload.userId)).thenResolve(user)
+    const findingUser: FindingUser = instance(mockFindingUser)
+
+    const mockInvalidatingToken: InvalidatingToken = mock(TokenRepository)
+    when(mockInvalidatingToken.invalidate(token)).thenResolve()
+    const invalidatingToken: InvalidatingToken = instance(mockInvalidatingToken)
+
+    const testClass = new Logout(invalidatingToken, findingUser)
+    await testClass.logout(jwtPayload, token)
+
+    verify(mockFindingUser.findById(userId)).never()
+    verify(mockInvalidatingToken.invalidate(token)).once()
+  })
+  it('should succeed when invalidate a all token for a user', async () => {
+    const jwtPayload = {
+      userId: userId,
+    }
+    const token = faker.datatype.number(6).toString()
+
+    const mockFindingUser: FindingUser = mock(UserRepository)
+    when(mockFindingUser.findById(userId)).thenResolve(user)
+    const findingUser: FindingUser = instance(mockFindingUser)
+
+    const mockInvalidatingToken: InvalidatingToken = mock(TokenRepository)
+    when(mockInvalidatingToken.invalidate(token, user)).thenResolve()
+    const invalidatingToken: InvalidatingToken = instance(mockInvalidatingToken)
+
+    const testClass = new Logout(invalidatingToken, findingUser)
+    await testClass.logout(jwtPayload, token, true)
+
+    verify(mockFindingUser.findById(userId)).once()
+    verify(mockInvalidatingToken.invalidate(token, user)).once()
+  })
+  it('should fail when invalidate a single token', async () => {
+    const jwtPayload = {
+      userId: userId,
+    }
+    const token = faker.datatype.number(6).toString()
+
+    const mockFindingUser: FindingUser = mock(UserRepository)
+    when(mockFindingUser.findById(jwtPayload.userId)).thenResolve(user)
+    const findingUser: FindingUser = instance(mockFindingUser)
+
+    const mockInvalidatingToken: InvalidatingToken = mock(TokenRepository)
+    when(mockInvalidatingToken.invalidate(token)).thenReject(
+      new Error(InvalidatingTokenErrorsTypes.NOT_FOUND)
+    )
+    const invalidatingToken: InvalidatingToken = instance(mockInvalidatingToken)
+
+    const testClass = new Logout(invalidatingToken, findingUser)
+    try {
+      await testClass.logout(jwtPayload, token)
+    } catch (error) {
+      expect((error as Error).message).to.be.eql(
+        LogoutUserErrorsTypes.WRONG_CREDENTIAL
+      )
+      verify(mockFindingUser.findById(userId)).never()
+      verify(mockInvalidatingToken.invalidate(token)).once()
+    }
+  })
+  it('should fail when finding a user', async () => {
+    const jwtPayload = {
+      userId: userId,
+    }
+    const token = faker.datatype.number(6).toString()
+
+    const mockFindingUser: FindingUser = mock(UserRepository)
+    when(mockFindingUser.findById(jwtPayload.userId)).thenReject(
+      new Error(FindingUserErrorsTypes.NOT_FOUND)
+    )
+    const findingUser: FindingUser = instance(mockFindingUser)
+
+    const mockInvalidatingToken: InvalidatingToken = mock(TokenRepository)
+    when(mockInvalidatingToken.invalidate(token)).thenResolve()
+    const invalidatingToken: InvalidatingToken = instance(mockInvalidatingToken)
+
+    const testClass = new Logout(invalidatingToken, findingUser)
+    try {
+      await testClass.logout(jwtPayload, token)
+    } catch (error) {
+      expect((error as Error).message).to.be.eql(
+        LogoutUserErrorsTypes.WRONG_CREDENTIAL
+      )
+      verify(mockFindingUser.findById(userId)).once()
+      verify(mockInvalidatingToken.invalidate(token)).never()
+    }
+  })
+})
