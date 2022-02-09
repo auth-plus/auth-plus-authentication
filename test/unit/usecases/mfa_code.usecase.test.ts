b42fa1654e568a9b53a51ee63768bac64898a771
AndrewHanasiro <andrewkhanasiro@gmail.com>
2022-02-09 18:43:28 -0300

    test: add case of fail on mfa_code

--- test/unit/usecases/mfa_code.usecase.test.ts
+++ test/unit/usecases/mfa_code.usecase.test.ts
@@ -10,7 +10,11 @@ import { TokenRepository } from '../../../src/core/providers/token.repository'
 import { UserRepository } from '../../../src/core/providers/user.repository'
 import { CreatingMFACode } from '../../../src/core/usecases/driven/creating_mfa_code.driven'
 import { CreatingToken } from '../../../src/core/usecases/driven/creating_token.driven'
-import { FindingMFA } from '../../../src/core/usecases/driven/finding_mfa.driven'
+import {
+  FindingMFA,
+  FindingMFAErrors,
+  FindingMFAErrorsTypes,
+} from '../../../src/core/usecases/driven/finding_mfa.driven'
 import { FindingMFACode } from '../../../src/core/usecases/driven/finding_mfa_code.driven'
 import { FindingUser } from '../../../src/core/usecases/driven/finding_user.driven'
 import { ValidatingCode } from '../../../src/core/usecases/driven/validating_code.driven'
@@ -29,15 +33,16 @@ describe('mfa code usecase', function () {
     name,
     email,
     info: {
-      deviceId: null,
-      googleAuth: null,
+      deviceId: faker.datatype.uuid(),
+      googleAuth: faker.datatype.uuid(),
       phone: phone,
     },
   }
   const mfaList = [{ id: faker.datatype.uuid(), strategy: Strategy.EMAIL }]
-  const strategy = Strategy.EMAIL
 
   it('should succeed when creating a mfa code', async () => {
+    const strategy = Strategy.EMAIL
+
     const mockCreatingMFACode: CreatingMFACode = mock(MFACodeRepository)
     when(
       mockCreatingMFACode.creatingCodeForStrategy(userId, strategy)
@@ -74,7 +79,9 @@ describe('mfa code usecase', function () {
     expect(response).to.eql(hash)
   })
 
-  it('should succeed when finding a mfa code', async () => {
+  it('should succeed when finding a mfa code for strategy EMAIL', async () => {
+    const strategy = Strategy.EMAIL
+
     const mockCreatingMFACode: CreatingMFACode = mock(MFACodeRepository)
     const creatingMFACode: CreatingMFACode = instance(mockCreatingMFACode)
 
@@ -124,8 +131,120 @@ describe('mfa code usecase', function () {
     verify(mockFindingMFACode.findByHash(hash)).once()
     verify(mockFindingUser.findById(userId)).once()
     verify(mockCreatingToken.create(user)).once()
+    verify(mockValidatingCode.validate(code, code)).once()
     expect(response.id).to.eql(user.id)
     expect(response.name).to.eql(user.name)
     expect(response.email).to.eql(user.email)
+  })
+  it('should succeed when finding a mfa code for strategy GA', async () => {
+    const strategy = Strategy.GA
+    const mockCreatingMFACode: CreatingMFACode = mock(MFACodeRepository)
+    const creatingMFACode: CreatingMFACode = instance(mockCreatingMFACode)
+
+    const mockFindingMFACode: FindingMFACode = mock(MFACodeRepository)
+    when(mockFindingMFACode.findByHash(hash)).thenResolve({
+      userId,
+      code,
+      strategy,
+    })
+    const findingMFACode: FindingMFACode = instance(mockFindingMFACode)
+
+    const mockFindingUser: FindingUser = mock(UserRepository)
+    when(mockFindingUser.findById(userId)).thenResolve(user)
+    const findingUser: FindingUser = instance(mockFindingUser)
+
+    const mockCreatingToken: CreatingToken = mock(TokenRepository)
+    when(mockCreatingToken.create(user)).thenReturn(token)
+    const creatingToken: CreatingToken = instance(mockCreatingToken)
+
+    const mockValidatingCode: ValidatingCode = mock(MFACodeRepository)
+    if (user.info.googleAuth) {
+      when(
+        mockValidatingCode.validateGA(code, user.info.googleAuth)
+      ).thenReturn()
+    }
+    const validatingCode: ValidatingCode = instance(mockValidatingCode)
+
+    const mockFindingMFA: FindingMFA = mock(MFARepository)
+    when(
+      mockFindingMFA.findMFAByUserIdAndStrategy(userId, strategy)
+    ).thenResolve({
+      id: faker.datatype.uuid(),
+      userId,
+      strategy,
+    })
+    const findingMFA: FindingMFA = instance(mockFindingMFA)
+
+    const testClass = new MFACode(
+      creatingMFACode,
+      findingMFACode,
+      findingUser,
+      creatingToken,
+      validatingCode,
+      findingMFA
+    )
+    const response = await testClass.find(hash, code)
+
+    verify(
+      mockCreatingMFACode.creatingCodeForStrategy(anything(), anything())
+    ).never()
+    verify(mockFindingMFACode.findByHash(hash)).once()
+    if (user.info.googleAuth) {
+      verify(mockValidatingCode.validateGA(code, user.info.googleAuth)).once()
+    }
+    verify(mockFindingUser.findById(userId)).once()
+    verify(mockCreatingToken.create(user)).once()
+    expect(response.id).to.eql(user.id)
+    expect(response.name).to.eql(user.name)
+    expect(response.email).to.eql(user.email)
+  })
+  it('should fail when finding a mfa code', async () => {
+    const strategy = Strategy.EMAIL
+
+    const mockCreatingMFACode: CreatingMFACode = mock(MFACodeRepository)
+    const creatingMFACode: CreatingMFACode = instance(mockCreatingMFACode)
+
+    const mockFindingMFACode: FindingMFACode = mock(MFACodeRepository)
+    when(mockFindingMFACode.findByHash(hash)).thenResolve({
+      userId,
+      code,
+      strategy,
+    })
+    const findingMFACode: FindingMFACode = instance(mockFindingMFACode)
+
+    const mockFindingUser: FindingUser = mock(UserRepository)
+    when(mockFindingUser.findById(userId)).thenResolve(user)
+    const findingUser: FindingUser = instance(mockFindingUser)
+
+    const mockCreatingToken: CreatingToken = mock(TokenRepository)
+    when(mockCreatingToken.create(user)).thenReturn(token)
+    const creatingToken: CreatingToken = instance(mockCreatingToken)
+
+    const mockValidatingCode: ValidatingCode = mock(MFACodeRepository)
+    when(mockValidatingCode.validate(code, code)).thenReturn()
+    const validatingCode: ValidatingCode = instance(mockValidatingCode)
+
+    const mockFindingMFA: FindingMFA = mock(MFARepository)
+    when(
+      mockFindingMFA.findMFAByUserIdAndStrategy(userId, strategy)
+    ).thenReject(new FindingMFAErrors(FindingMFAErrorsTypes.NOT_FOUND))
+    const findingMFA: FindingMFA = instance(mockFindingMFA)
+
+    const testClass = new MFACode(
+      creatingMFACode,
+      findingMFACode,
+      findingUser,
+      creatingToken,
+      validatingCode,
+      findingMFA
+    )
+    try {
+      await testClass.find(hash, code)
+    } catch (error) {
+      verify(
+        mockCreatingMFACode.creatingCodeForStrategy(anything(), anything())
+      ).never()
+      expect((error as Error).message).to.eql(FindingMFAErrorsTypes.NOT_FOUND)
+    }
   })
 })
