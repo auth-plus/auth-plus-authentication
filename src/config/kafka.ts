dcddb19a03b869941e5b337d21d8780e3e32bf09
Andrew Hanasiro <andrewkhanasiro@gmail.com>
2022-12-13 21:48:00 -0300

    Feature/move observability (#111)
    * chore: remove observability tools
    
    * chore: remove file from tsconfig
    
    * fix: version node

--- src/config/kafka.ts
+++ src/config/kafka.ts
@@ -1,10 +1,6 @@
 import { Kafka, logLevel, CompressionTypes } from 'kafkajs'
 
-import { KafkaTopic } from '../presentation/messaging/app'
-
 import env from './enviroment_config'
-import logger from './logger'
-import { metric } from './metric'
 
 const kafka = new Kafka({
   logLevel: logLevel.ERROR,
@@ -13,8 +9,8 @@ const kafka = new Kafka({
   brokers: [`${env.broker.host}:${env.broker.port}`],
 })
 
-const TOPIC_LIST = ['health', 'organization-create']
-const consumer = kafka.consumer({ groupId: env.app.name })
+type TOPIC = 'SENT_EMAIL' | 'SENT_PUSH_NOTIFICATION'
+const TOPIC_LIST: Array<TOPIC> = ['SENT_EMAIL', 'SENT_PUSH_NOTIFICATION']
 
 export async function configKafka() {
   const admin = kafka.admin()
@@ -28,34 +24,7 @@ export async function configKafka() {
   await admin.disconnect()
 }
 
-export const consume = async (
-  callback: (topic: KafkaTopic, content: Record<string, any>) => Promise<void>
-) => {
-  await consumer.connect()
-  const promiseList = TOPIC_LIST.map((tpc) => {
-    return consumer.subscribe({ topic: tpc, fromBeginning: true })
-  })
-  await Promise.all(promiseList)
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      //metrics
-      const hour = new Date().getHours()
-      metric.histogramObserve('histogram_request', hour)
-
-      //tracer
-      const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
-      logger.info(`- ${prefix} ${message.key}#${message.value}`)
-
-      if (!message.value) {
-        throw new Error('no payload')
-      }
-      const content = JSON.parse(message.value?.toString())
-      await callback(topic as KafkaTopic, content as Record<string, any>)
-    },
-  })
-}
-
-export const produce = async (topic: string, payload: Record<string, any>) => {
+export const produce = async (topic: TOPIC, payload: Record<string, any>) => {
   const producer = kafka.producer()
   await producer.connect()
   await producer.send({
