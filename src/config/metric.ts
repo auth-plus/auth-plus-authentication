2bcef0eb6899cdebe6cb341052d9be8407a513db
Andrew Hanasiro <andrewkhanasiro@gmail.com>
2022-12-21 17:46:42 -0300

    chore: replace prometheus lib (#113)

--- src/config/metric.ts
+++ src/config/metric.ts
@@ -1,8 +1,35 @@
-import { MetricsSingleton } from '@auth-plus/metrics'
+import {
+  Registry,
+  collectDefaultMetrics,
+  Counter,
+  Histogram,
+} from 'prom-client'
 
-export const metric = MetricsSingleton.getInstance({
-  type: 'prometheus',
-  config: {},
+import env from './enviroment_config'
+
+collectDefaultMetrics({ prefix: env.app.name.replaceAll('-', '_') })
+
+export const registry = new Registry()
+
+// Create all custom metrics below
+export const histogramRequest = new Histogram({
+  name: 'histogram_request',
+  help: 'distribution of request per hour',
+  buckets: [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23,
+  ],
+  registers: [registry],
 })
 
-metric.createHistogram('histogram_request', 'distribution of request per hour')
+export const counter200 = new Counter({
+  name: 'counter_succees',
+  help: 'count of succees',
+  registers: [registry],
+})
+
+export const counter500 = new Counter({
+  name: 'counter_fail',
+  help: 'count of fail',
+  registers: [registry],
+})
