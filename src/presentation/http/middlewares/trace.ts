fafe7bc74bf5c4e4a168da38d5a9eae140f9bc5b
AndrewHanasiro <andrewkhanasiro@gmail.com>
2022-12-29 23:06:14 -0300

    feat: Zipkin

---
+++ src/presentation/http/middlewares/trace.ts
@@ -0,0 +1,23 @@
+import { trace } from '@opentelemetry/api'
+import { Request, Response, NextFunction } from 'express'
+
+import env from '../../../config/enviroment_config'
+
+export function traceMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  const tracer = trace.getTracer(env.app.name)
+  tracer.startActiveSpan('main', (span) => {
+    span.setAttribute('HTTP_METHOD', req.method)
+    span.setAttribute('HTTP_URL', req.path)
+    next()
+    span.setAttribute('HTTP_STATUS_CODE', res.statusCode)
+    span.addEvent('HTTP_FINISHED', {
+      'response.body': JSON.stringify(res.body),
+      'request.body': JSON.stringify(req.body),
+    })
+    span.end()
+  })
+}
---
+++ src/presentation/tracing.ts
@@ -0,0 +1,35 @@
+import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api'
+import { ZipkinExporter } from '@opentelemetry/exporter-zipkin'
+import { registerInstrumentations } from '@opentelemetry/instrumentation'
+import { Resource } from '@opentelemetry/resources'
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base'
+import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node'
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
+
+import env from '../config/enviroment_config'
+
+// For troubleshooting, set the log level to DiagLogLevel.DEBUG
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO)
+
+// Optionally register instrumentation libraries
+registerInstrumentations({
+  instrumentations: [],
+})
+
+const resource = Resource.default().merge(
+  new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: env.app.name,
+    [SemanticResourceAttributes.SERVICE_VERSION]: '0.1.0',
+  })
+)
+
+const provider = new NodeTracerProvider({
+  resource: resource,
+})
+const exporter = new ZipkinExporter({
+  url: 'http://zipkin:9411/api/v2/spans',
+})
+const processor = new BatchSpanProcessor(exporter)
+provider.addSpanProcessor(processor)
+
+provider.register()
